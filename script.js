f9348676297cce49f2f146ea44196fd77d4a4750
isabelleciussantos-ops <isabelle.cius.santos@escola.pr.gov.br>
2026-08-31 18:37:37 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+    document.getElementById("mensagem").textContent =
+        "Você consegue! Com organização e dedicação, cada dia de estudo é um passo mais perto dos seus objetivos. 💗";
+}
